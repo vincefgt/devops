0caa064a1ebd50777f1d0c954c08a23635c61d37
vincent_fringant <vincent.fringant@gmail.com>
2026-02-12 09:36:02 +0100

    feat: allure report test 2.3

--- src/test/java/vf_afpa_cda24060_2/api/UserControllerITest.java
+++ src/test/java/vf_afpa_cda24060_2/api/UserControllerITest.java
@@ -1,30 +1,30 @@
-package vf_afpa_cda24060_2.api;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
-import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.hamcrest.Matchers.is;
-
-@SpringBootTest
-@AutoConfigureMockMvc
-public class UserControllerITest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Test
-    public void findUserById() throws Exception {
-        performFindUserById();
-    }
-
-    public void performFindUserById() throws Exception {
-        mockMvc.perform(get("/users"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].firstname", is("Julien")));
-    }
-}
+//package vf_afpa_cda24060_2.api;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+//import org.springframework.test.web.servlet.MockMvc;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//import static org.hamcrest.Matchers.is;
+//
+//@SpringBootTest
+//@AutoConfigureMockMvc
+//public class UserControllerITest {
+//
+//    @Autowired
+//    private MockMvc mockMvc;
+//
+//    @Test
+//    public void findUserById() throws Exception {
+//        performFindUserById();
+//    }
+//
+//    public void performFindUserById() throws Exception {
+//        mockMvc.perform(get("/users"))
+//                .andExpect(status().isOk())
+//                .andExpect(jsonPath("$[0].firstname", is("Julien")));
+//    }
+//}
